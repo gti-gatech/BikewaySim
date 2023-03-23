39aa8a9a09d457d7743164dac32d28f8ea9db472
Reid Passmore <tpassmore6@gatech.edu>
2023-03-23 10:17:21 -0700

    transfer back to desktop

--- network_reconcile.py
+++ network_reconcile.py
@@ -10,7 +10,7 @@ from shapely.ops import LineString, Point, MultiPoint
 from pathlib import Path
 import geopandas as gpd
 
-def add_attributes(base_links, join_links, join_name, buffer_ft):
+def add_attributes(base_links, join_links, join_name, buffer_ft, non_network=False):
     
     #give base_links a temp column so each row has unique identifier
     # A_B doesn't always work because there might be duplicates from the split links step
@@ -28,13 +28,16 @@ def add_attributes(base_links, join_links, join_name, buffer_ft):
     #buffer the join links by tolerance_ft
     buffered_links.geometry = buffered_links.buffer(buffer_ft)
     
-    #drop unique columns
-    buffered_links.drop(columns=[f'{join_name}_A',f'{join_name}_B',f'{join_name}_A_B'],inplace=True)
+    if non_network == True:
+        dissolved_links = buffered_links
+    else:
+        #drop unique columns
+        buffered_links.drop(columns=[f'{join_name}_A',f'{join_name}_B',f'{join_name}_A_B'],inplace=True)
 
-    #dissolve by attributes in join links
-    cols = [x for x in buffered_links.columns if x != 'geometry']
-    dissolved_links = buffered_links.dissolve(cols)
-    
+        #dissolve by attributes in join links
+        cols = [x for x in buffered_links.columns if x != 'geometry']
+        dissolved_links = buffered_links.dissolve(cols)
+        
     #intersect join buffer and base links
     overlapping_links = gpd.overlay(base_links, dissolved_links, how='intersection')
     
@@ -55,10 +58,10 @@ def add_attributes(base_links, join_links, join_name, buffer_ft):
     
     return base_links, buffered_links, dissolved_links, match_links, overlapping_links
 
-def add_osm_attr(liknks,fp):
+def add_osm_attr(links,attr_fp):
 
     #bring in attribute data
-    attr = pd.read_pickle(fp / Path('osm.pkl'))
+    attr = pd.read_pickle(attr_fp)
 
     #attach attribute data to filtered links
     links = pd.merge(links,attr,on='osm_A_B')
@@ -132,5 +135,107 @@ def add_osm_attr(liknks,fp):
     links.loc[links['sidewalk'].isin(sidewalks),'sidewalk_pres'] = 'yes'
     links.loc[links['sidewalk'].isin([None,'no','none']),'sidewalk_pres'] = 'no'
     links.drop(columns=['sidewalk'],inplace=True)
+
+    return links
+
+def add_here_attr(links,attr_fp):
+
+    #revelant attributes
+    here_attr = ['here_A_B','ST_NAME','SPEED_CAT','LANE_CAT','DIR_TRAVEL','FUNC_CLASS']
+
+    #bring in attribute data
+    attr = pd.read_pickle(attr_fp)[here_attr]
+
+    #attach attribute data to filtered links
+    links = pd.merge(links,attr,on='here_A_B')
+
+    # speed categories
+    here_speed_bins = {
+        '1': '> 30', # '> 80 MPH',
+        '2': '> 30', # '65-80 MPH',
+        '3': '> 30', # '55-64 MPH',
+        '4': '> 30', # '41-54 MPH',
+        '5': '> 30', # '31-40 MPH',
+        '6': '25-30', # '21-30 MPH',
+        '7': '< 25', # '6-20 MPH',
+        '8': '< 25' # '< 6 MPH'
+        }
+    links['SPEED_CAT'] = links['SPEED_CAT'].map(here_speed_bins)
+
+    # number of lanes
+    here_lane_bins = {
+        '1': '1', # 'one lane',
+        '2': '2-3', # 'two or three lanes',
+        '3': '> 4' # 'four or more'
+        }
+    links['LANE_CAT'] = links['LANE_CAT'].map(here_lane_bins)
+
+    # road directionality
+    here_oneway_bins = {
+        'B':'both', # Both Directions
+        'F':'oneway', # From Reference Node
+        'T':'wrongway', # To Reference Node
+        'N': 'NA' # Closed in both directions
+        }
+    links['DIR_TRAVEL'] = links['DIR_TRAVEL'].map(here_oneway_bins)
+
+    # here functional class (does not correspond to FHWA or HFCS)
+    func_class = {
+        '1':'highways',
+        '2':'major arterials',
+        '3':'collectors/minor arterials',
+        '4':'collectors/minor atrerials',
+        '5':'local'
+        }
+    links['FUNC_CLASS'] = links['FUNC_CLASS'].map(func_class)
+
+    return links
+
+def add_abm_attr(links,attr_fp):
+    
+    abm_cols = ['SPEEDLIMIT','oneway']
+
+    #bring in attribute data
+    attr = pd.read_pickle(attr_fp)
+
+    #attach attribute data to filtered links
+    links = pd.merge(links,attr,on='here_A_B')
+    
+    #speed
+    links['SPEEDLIMIT']
+
+    conditions = [
+        (links['SPEEDLIMIT'] > 80),
+        (links['SPEEDLIMIT'] >= 65) & (links['SPEEDLIMIT'] < 80),
+        (links['SPEEDLIMIT'] >= 55) & (links['SPEEDLIMIT'] < 64),
+        (links['SPEEDLIMIT'] >= 41) & (links['SPEEDLIMIT'] < 54),
+        (links['SPEEDLIMIT'] >= 31) & (links['SPEEDLIMIT'] < 40),
+        (links['SPEEDLIMIT'] >= 21) & (links['SPEEDLIMIT'] < 30),
+        (links['SPEEDLIMIT'] >= 6) & (links['SPEEDLIMIT'] < 20),
+        (links['SPEEDLIMIT'] < 6)
+    ]
+    values = ['> 80 MPH', '65-80 MPH', '55-64 MPH', '41-54 MPH', '31-40 MPH', '21-30 MPH', '6-20 MPH', '< 6 MPH']
+
+    links['SPEEDLIMIT'] = np.select(conditions, values)
+
+    #oneway
+    links['oneway'] = links['two_way'] == False
+
+    return links
+
+def add_arc_bike(links):
+
+    links = links[['facil','geometry']]
+
+    links['pbl']=0
+    links.loc[links['facil']=='Protected Bike Lane','pbl'] = 1
+
+    links['bl']=0
+    links.loc[links['facil']=='Bike Lane','bl'] = 1
+
+    links['mu']=0
+    links.loc[(links['facil']=='Multi-Use Path'),'mu'] = 1
+
+    links.drop(columns=['facil'],inplace=True)
 
     return links
--- od_pair_generation.py
+++ od_pair_generation.py
@@ -11,143 +11,54 @@ import matplotlib.pyplot as plt
 import random
 import os
 from pathlib import Path
+import itertools
 
-#make directory/pathing more intuitive later
-user_directory = os.fspath(Path.home()) #get home directory and convert to path string
-file_directory = "\Documents\BikewaySimData" #directory of bikewaysim network processing code
+def all_pairs(settings):
 
-#change this to where you stored this folder
-os.chdir(user_directory+file_directory)
+    studyarea_name = settings['studyarea_name']
+    studyarea_fp = settings['studyarea_fp']
+    tazs_fp = settings['tazs_fp']
 
-#%% Method 1 random OD pairs
-def get_random_OD_pairs(poly, num_of_pairs):
-    
-     #simplify gdf to poly
-     poly_simp = poly.geometry[0]
-     minx, miny, maxx, maxy = poly_simp.bounds #get the min and max extents of the polygon
-     
-     #initialize empty lists to contain od pairs
-     trip_id = []
-     origin_geo = []
-     dest_geo = []
-     
-     x = 1
-     while x < (num_of_pairs+1):
-         y = Point(random.uniform(minx, maxx), random.uniform(miny, maxy))
-         z = Point(random.uniform(minx, maxx), random.uniform(miny, maxy))
-         if poly_simp.contains(y) and poly_simp.contains(z):
-             trip_id.append(x)
-             origin_geo.append(y)
-             dest_geo.append(z)
-             x += 1
-         else:
-             x = x
-             
-     #turn lists into GeoSeries
-     origin_geo = gpd.GeoSeries(origin_geo)
-     dest_geo = gpd.GeoSeries(dest_geo)
-     
-     points = pd.DataFrame({'trip_id':trip_id,'ori_lat':origin_geo.geometry.y,'ori_lon':origin_geo.x,
-                                'dest_lat':dest_geo.y,'dest_lon':dest_geo.x, 'origin_geo':origin_geo})
-     
-     #export         
-     points.to_csv('od_pairs/od_pairs.csv')
-     
-     #plotting
-     fig, ax = plt.subplots()
+    #import
+    studyarea = gpd.read_file(studyarea_fp)
+    tazs = gpd.read_file(studyarea_fp / Path(f'{studyarea_name}/base_layers.gpkg'),mask=studyarea,layer='studyarea')[[settings['tazs_id_col'],'geometry']]]
+    tazs.to_crs(studyarea.crs,inplace=True)
 
-     ax.set_aspect('equal')
+    #make sure ids are columns
+    tazs['FID_1'] = tazs['FID_1'].astype(str)
 
-     poly.plot(ax=ax, color ='white', edgecolor='black')
-     origin_geo.plot(ax=ax, marker='o', color='red', markersize=5)
-     dest_geo.plot(ax=ax, marker='o', color='green', markersize=5)
-     plt.show();
-     
-     return points
+    #export
+    tazs.to_file(studyarea_fp,layer='tazs_polygons')
 
-#study_area = gpd.read_file(r'Processed_Shapefiles/study_areas/study_area.shp').to_crs(epsg=4326)
+    #get centroids
+    tazs.geometry = tazs.centroid
 
-#od_pairs = get_random_OD_pairs(study_area,1000)
+    #export again
+    tazs.to_file(studyarea_fp,layer='tazs_centroids')
 
+    #get lat and lon columns
+    tazs.to_crs('epsg:4326',inplace=True)
+    tazs['lat'] = tazs.geometry.y
+    tazs['lon'] = tazs.geometry.x
 
-#%%method 2, TAZ centroids, deprecated
-#abm_nodes = gpd.read_file(r'processed_shapefiles/abm/abm_bikewaysim_base_nodes.geojson')
+    #get permuations of taz ids
+    perms = list(itertools.permutations(tazs[settings['tazs_id_col']],2))
 
-# bikewaysim_studyarea = gpd.read_file(r'processed_shapefiles/study_areas/study_area.geojson').to_crs('epsg:4326')
+    #make df
+    od_pairs = pd.DataFrame(columns=['trip_id','ori_id','dest_id','ori_lat','ori_lon','dest_lat','dest_lon'])
 
-# tazs = gpd.read_file(r'base_shapefiles/arc/Model_Traffic_Analysis_Zones_2020/Model_Traffic_Analysis_Zones_2020.shp',
-#                      mask=bikewaysim_studyarea)
+    od_pairs['perms'] = perms
+    #seperate into two columns
 
+    od_pairs = pd.merge(od_pairs,tazs,left_on='ori_id',right_on=settings['tazs_id_col'])
+    od_pairs.rename(columns:{'lat':'ori_lat','lon':'ori_lon'},inplace=True)
 
+    od_pairs = pd.merge(od_pairs,tazs,left_on='dest_id',right_on=settings['tazs_id_col'])
+    od_pairs.rename(columns:{'lat':'dest_lat','lon':'dest_lon'},inplace=True)
 
-#tazs.to_file('tu_delft/tazs.gpkg',driver='GPKG',layer='tazs')
+    od_pairs['trip_id'] = od_pairs['ori_id'] + '_' + od_pairs['dest_id']
 
-tazs = gpd.read_file('trb2023/tazs.gpkg',layer='centroids') 
+    #export
+    od_pairs.to_csv(studyarea_fp / Path(f'{studyarea_name}/od_pairs.csv'), index = False)
 
-#get centroid
-#tazs['geometry'] = tazs.geometry.centroid
-
-
-tazs['FID_1'] = tazs['FID_1'].astype(str)
-
-#export centroids
-#tazs.to_file('tu_delft/tazs.gpkg',driver='GPKG',layer='centroids')
-
-
-#%%
-#create list of od pairs
-list_of_ids = tazs['FID_1'].tolist()
-
-ori_id = []
-dest_id = []
-
-#https://stackoverflow.com/questions/28757389/pandas-loc-vs-iloc-vs-at-vs-iat
-for x in list_of_ids:
-    for y in list_of_ids:
-        if y != x:
-            ori_id.append(x)
-            dest_id.append(y)
-od_pairs = pd.DataFrame({'ori_id':ori_id,'dest_id':dest_id})          
-            
-
-# merge geometry info back in and create other columns
-od_pairs = pd.merge(od_pairs,tazs[['FID_1','geometry']], left_on = 'ori_id', right_on = 'FID_1', how='left')
-od_pairs = od_pairs.rename(columns={'geometry':'ori_geometry'}).drop(columns={'FID_1'})
-
-od_pairs = pd.merge(od_pairs,tazs[['FID_1','geometry']], left_on = 'dest_id', right_on = 'FID_1', how='left')
-od_pairs = od_pairs.rename(columns={'geometry':'dest_geometry'}).drop(columns={'FID_1'})
-
-od_pairs = od_pairs.set_geometry('ori_geometry').to_crs(epsg=4326)
-od_pairs['ori_lat'] = od_pairs.geometry.y
-od_pairs['ori_lon'] = od_pairs.geometry.x
-
-od_pairs = od_pairs.set_geometry('dest_geometry').to_crs(epsg=4326)
-od_pairs['dest_lat'] = od_pairs.geometry.y
-od_pairs['dest_lon'] = od_pairs.geometry.x
-
-od_pairs['trip_id'] = od_pairs['ori_id'] + '_' + od_pairs['dest_id']
-od_pairs = od_pairs[['trip_id','ori_id','dest_id','ori_lat','ori_lon','dest_lat','dest_lon']]
-
-#export
-od_pairs.to_csv('bikewaysim_outputs/samples_in/all_tazs.csv', index = False)
-
-
-#%% alt method 2
-
-#tazs = gpd.read_file('demonstration_viz/tazs.geojson', ignore_geometry=True)
-
-#get rid of excess columns
-
-
-#%% create OD folders
-
-#export         
-#od_pairs.to_csv('od_pairs/od_pairs.csv', index = False)
-
-#%% for viz
-
-#for aggregate impedance
-#single_taz_to_all = od_pairs[od_pairs['ori_id'] == '538']
-#
-#export
-#single_taz_to_all.to_csv('bikewaysim_outputs/samples_in/single_taz_to_all.csv', index = False)
+#TODO have other method?
